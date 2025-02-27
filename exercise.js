1367dc8fb5c6912c75dae7318e560ac3ec642fcc
Gina-4 <Georgina.Riri4@gmail.com>
2025-02-27 15:18:14 +1300

    JS intro

---
+++ exercise.js
@@ -0,0 +1,23 @@
+let CurrentOutsideTemp="60 degrees";
+console.log(CurrentOutsideTemp)
+let FavoritePizza="margherita";
+console.log(FavoritePizza)
+let NumberCharacterString="26";
+console.log(NumberCharacterString)
+let Strings = CurrentOutsideTemp +" "+ FavoritePizza; 
+console.log(Strings)
+let names="Gina";
+console.log(names)
+let age="10";
+console.log(age)
+let namesAndAge = names +" "+ age;
+console.log(namesAndAge)
+console.log(`my name is ${names} i am ${age}`)
+
+
+
+
+
+
+
+
